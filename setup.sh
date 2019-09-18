665f631283a7aa922e2b5881d081a2a87173c915
Kyran Burraston <kyran.burraston@gmail.com>
2019-09-18 15:32:24 +0200

    chore: Remove slate, not using it, update setup file

--- setup.sh
+++ setup.sh
@@ -7,24 +7,18 @@ ln -s $HOME/dev/dot-files/zsh/.zshrc $HOME
 ln -s $HOME/dev/dot-files/zsh/.functions.zsh $HOME
 ln -s $HOME/dev/dot-files/zsh/.prompt.zsh $HOME
 
-
 ln -s $HOME/dev/dot-files/.inputrc $HOME
-ln -s $HOME/dev/dot-files/.slate $HOME
 ln -s $HOME/dev/dot-files/.tmux.conf $HOME
 ln -s $HOME/dev/dot-files/.ackrc $HOME
 ln -s $HOME/dev/dot-files/.gitconfig $HOME
-
-ln -s $HOME/dev/dot-files/.vimrc $HOME
+ln -s $HOME/dev/dot-files/.hammerspoon $HOME
 
 ln -s $HOME/dev/dot-files/ranger $HOME/.config
+ln -s $HOME/dev/dot-files/karabiner $HOME/.config
+ln -s $HOME/dev/dot-files/nvim $HOME/.config
 
 # Sublime Text
 ln -s $HOME/dev/dot-files/Preferences.sublime-settings $HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
 ln -s $HOME/dev/dot-files/snippets $HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
 ln -s $HOME/dev/dot-files/Default\ \(OSX\).sublime-keymap $HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
 
-# Karabiner-Elements
-ln -s $HOME/dev/dot-files/karabiner $HOME/.config
-
-# Hammerspoon
-ln -s $HOME/dev/dot-files/.hammerspoon $HOME
