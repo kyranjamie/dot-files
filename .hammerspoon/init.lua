2b043d5b2aa3ff084d77293cc6cd765c263a2f1a
kyranjamie <kyran.burraston@gmail.com>
2020-09-07 19:45:08 +0200

    feat: add new VSCode settings

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -16,16 +16,21 @@ hs.hotkey.bind(hyper, "R", function()
   hs.notify.new({title="Hammerspoon config reloaded", informativeText="Manually via keyboard shortcut"}):send()
 end)
 
+
+-- Taken
+-- Hyper+P = 1password
+
 hs.hotkey.bind(hyper, 'A', function () hs.application.launchOrFocus("Google Chrome") end)
-hs.hotkey.bind(hyper, 'D', function () hs.application.launchOrFocus("Figma") end)
-hs.hotkey.bind(hyper, 'M', function () hs.application.launchOrFocus("Spotify") end)
-hs.hotkey.bind(hyper, 'E', function () hs.application.launchOrFocus("Visual Studio Code") end)
-hs.hotkey.bind(hyper, 'G', function () hs.application.launchOrFocus("Telegram") end)
-hs.hotkey.bind(hyper, 'F', function () hs.application.launchOrFocus("Finder") end)
-hs.hotkey.bind(hyper, 'W', function () hs.application.launchOrFocus("WhatsApp") end)
-hs.hotkey.bind(hyper, 'S', function () hs.application.launchOrFocus("Slack") end)
+hs.hotkey.bind(hyper, 'B', function () hs.application.launchOrFocus("Electron") end)
 hs.hotkey.bind(hyper, 'C', function () hs.application.launchOrFocus("Calendar") end)
+hs.hotkey.bind(hyper, 'D', function () hs.application.launchOrFocus("Figma") end)
+hs.hotkey.bind(hyper, 'E', function () hs.application.launchOrFocus("Visual Studio Code") end)
+hs.hotkey.bind(hyper, 'F', function () hs.application.launchOrFocus("Finder") end)
+hs.hotkey.bind(hyper, 'G', function () hs.application.launchOrFocus("Telegram") end)
+hs.hotkey.bind(hyper, 'M', function () hs.application.launchOrFocus("Spotify") end)
+hs.hotkey.bind(hyper, 'S', function () hs.application.launchOrFocus("Slack") end)
 hs.hotkey.bind(hyper, 'T', function () hs.application.launchOrFocus("Microsoft To Do") end)
+hs.hotkey.bind(hyper, 'W', function () hs.application.launchOrFocus("WhatsApp") end)
 hs.hotkey.bind(hyper, 'Z', function () hs.application.launchOrFocus("zoom.us") end)
 
 function toggleFullScreen(s)
